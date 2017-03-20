702e426f23af902314cfe44f53cfeb2b7e3f314e
BrawnyJavier <brawnyjavier@gmail.com>
2017-03-20 18:10:30 -0400

    client with config on datamodel

---
+++ IntecBook/WebClient/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using IntecBook.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            using (var _context = new IntecBookContext())
+            {
+                _context.Day.Add(new IntecBook.classes.Day()
+                {
+                    Id = 2,
+                    Name = "Lunes"
+                });
+                _context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
